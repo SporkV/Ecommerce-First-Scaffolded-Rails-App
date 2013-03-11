f3ce7075c40a64c8f027d957fc91e0986912f9b3
Wally Glutton <stungeye@gmail.com>
2013-03-11 15:08:54 -0500

    Initial commit of a generic Rails app. Rails version 3.2.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EcommScaffold::Application.config.secret_token = '113658f166cdb534921fae30b5aaa4f5f3cc3ee1f28c41c774aec4a74944618cdb7a99d09c5c5b96335355a09e8517e1425c43702e9ca851f4d3114f94cbfb04'
